b78d8689af19a031f07586fb6a4ccf50e092cbf5
yessa <108473583+ayessaaa@users.noreply.github.com>
2026-05-20 01:09:41 +0800

    Initial Commit

---
+++ Rivet/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Rivet
+//
+//  Created by yessa on 5/20/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Rivet/RivetApp.swift
@@ -0,0 +1,17 @@
+//
+//  RivetApp.swift
+//  Rivet
+//
+//  Created by yessa on 5/20/26.
+//
+
+import SwiftUI
+
+@main
+struct RivetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
